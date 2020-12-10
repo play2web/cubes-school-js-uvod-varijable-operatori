00e9041e7b598376a91f6a63e11030990642615a
Miomir dancevic <rudnik@hotmail.com>
2020-12-10 10:19:33 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+// TASK 1
+var petersMoney = 3500;
+var jimsMoney = 5000;
+
+console.log('Peter has 3500$, and Jim has 5000$, print if Peter has more money than Jim');
+petersMoney > jimsMoney ? console.log('Peter have more money') : console.log('Peter does not have more money')
+
+
+// TASK 2
+var john = 38;
+var johnNewAge = 38 + 8;
+
+console.log('John is 38 years old. In 8 years John will be two times older than Mike. How old is Mike now');
+console.log('Mike current age ' + (johnNewAge / 2 - 8))
+
+
+// TASK 3
+var firstFlock = 78;
+var secondFlock= 57;
+firstFlock = firstFlock - 12;
+secondFlock =  secondFlock + 12 - 4;
+
+console.log('There are 78 birds in a first flock, and 57 birds in a second flock. 12 birds decided to move from the first flock to the second flock, and then 4 birds left second flock. Is the first flock larger or equal to the second one');
+firstFlock >= secondFlock ? console.log('First flock is larger or equal to second flock') : console.log('First flock is smaller from second flock')
+
